eebd7b56b41f1895f03f29b44ca3409450c24737
Danila Babanov <danila_babanov@yahoo.com>
2015-12-15 00:12:06 +0400

    Add username and login redirect

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214193029) do
+ActiveRecord::Schema.define(version: 20151214200217) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -50,9 +50,11 @@ ActiveRecord::Schema.define(version: 20151214193029) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
